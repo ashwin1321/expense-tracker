8866eb85346ac2ca3e698fda483a9da13fd56278
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-01-28 16:15:23 +0545

    layout created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Homepage from "./pages/Homepage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="container-fluid">
-      <h1 className="text-center">Hello</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </>
   );
 }
 
