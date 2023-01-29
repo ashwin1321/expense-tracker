523ce7b0d7a002aebfdaea660a19270fce078d92
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-01-29 19:27:59 +0545

    login/register page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Footer from "../components/layout/Footer";
-import Navbar from "../components/layout/Header";
+import Layout from "../components/layout/Layout";
 import { Form, Input } from "antd";
 import { Link } from "react-router-dom";
 
@@ -10,27 +9,26 @@ const Login = () => {
   };
   return (
     <>
-      <Navbar />
-      <div className=" register-page ">
-        <Form layout="vertical" onFinish={handleSubmit}>
-          <h1 className="txt">Please Login</h1>
-          <Form.Item label="Username" name="name">
-            <Input type="text" placeholder="Enter Username" />
-          </Form.Item>
-          <Form.Item label="Password" name="password">
-            <Input type="password" placeholder="Enter Password" />
-          </Form.Item>
-          <div className=" abc d-flex justify-content-between">
-            <Link to="/register" className="link">
-              Not a user? click here to register.
-            </Link>
-            <br />
-            <button className="btn btn-primary">Login</button>
-          </div>
-        </Form>
-      </div>
-
-      <Footer />
+      <Layout>
+        <div className=" register-page ">
+          <Form layout="vertical" onFinish={handleSubmit}>
+            <h1 className="txt">Please Login</h1>
+            <Form.Item label="Username" name="name">
+              <Input type="text" placeholder="Enter Username" />
+            </Form.Item>
+            <Form.Item label="Password" name="password">
+              <Input type="password" placeholder="Enter Password" />
+            </Form.Item>
+            <div className=" abc d-flex justify-content-between">
+              <Link to="/register" className="link">
+                Not a user? click here to register.
+              </Link>
+              <br />
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </Form>
+        </div>
+      </Layout>
     </>
   );
 };
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Footer from "../components/layout/Footer";
-import Navbar from "../components/layout/Header";
+import Layout from "../components/layout/Layout";
 import { Form, Input } from "antd";
 import { Link } from "react-router-dom";
 
@@ -11,32 +10,31 @@ const Register = () => {
 
   return (
     <>
-      <Navbar />
-      <div className=" register-page ">
-        <Form layout="vertical" onFinish={handleSubmit}>
-          {" "}
-          {/* antd doesn't have onSubmit */}
-          <h1 className="txt">Please Register</h1>
-          <Form.Item label="Username" name="name">
-            <Input type="text" placeholder="Enter Username" />
-          </Form.Item>
-          <Form.Item label="email" name="email">
-            <Input type="email" placeholder="Enter Email" />
-          </Form.Item>
-          <Form.Item label="Password" name="password">
-            <Input type="password" placeholder="Enter Password" />
-          </Form.Item>
-          <div className=" abc d-flex justify-content-between">
-            <Link to="/login" className="link">
-              Already Register? Click here to Login.
-            </Link>
-            <br />
-            <button className="btn btn-primary">Register</button>
-          </div>
-        </Form>
-      </div>
-
-      <Footer />
+      <Layout>
+        <div className=" register-page ">
+          <Form layout="vertical" onFinish={handleSubmit}>
+            {" "}
+            {/* antd doesn't have onSubmit */}
+            <h1 className="txt">Please Register</h1>
+            <Form.Item label="Username" name="name">
+              <Input type="text" placeholder="Enter Username" />
+            </Form.Item>
+            <Form.Item label="email" name="email">
+              <Input type="email" placeholder="Enter Email" />
+            </Form.Item>
+            <Form.Item label="Password" name="password">
+              <Input type="password" placeholder="Enter Password" />
+            </Form.Item>
+            <div className=" abc d-flex justify-content-between">
+              <Link to="/login" className="link">
+                Already Register? Click here to Login.
+              </Link>
+              <br />
+              <button className="btn btn-primary">Register</button>
+            </div>
+          </Form>
+        </div>
+      </Layout>
     </>
   );
 };
