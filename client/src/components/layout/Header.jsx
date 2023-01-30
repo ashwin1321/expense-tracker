cbc14b875c3dd984a301ec1f0da04eeb18c8a207
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-01-31 00:54:04 +0545

    complete

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -16,47 +16,46 @@ const Header = () => {
 
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
-        <div className="container-fluid">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarTogglerDemo01"
-            aria-controls="navbarTogglerDemo01"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <Link className="navbar-brand" to="#">
-              Expense<span className="text-success">Management</span>
-            </Link>
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <p className="nav-link active">{loginUser && loginUser.name}</p>
-              </li>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarTogglerDemo03"
+          aria-controls="navbarTogglerDemo03"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <Link className="navbar-brand" to="/">
+          Expense<span className="text-success">Management</span>
+        </Link>
 
-              <li className="nav-item">
-                <button
-                  className="btn btn-primary"
-                  onClick={() => {
-                    localStorage.removeItem("user");
-                    message.success("Logged out");
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul className="navbar-nav ms-auto mx-2">
+            <li className="text-white my-1 mx-2">
+              <p classNameName="nav-link  ">{loginUser && loginUser.name}</p>
+            </li>
+            <li>
+              <button
+                className="btn btn-primary my-2 my-sm-0"
+                onClick={() => {
+                  localStorage.removeItem("user");
+                  message.success("Logged out");
 
-                    navigate("/login");
-                  }}
-                >
-                  Logout
-                </button>
-              </li>
-            </ul>
-          </div>
+                  navigate("/login");
+                }}
+              >
+                Logout
+              </button>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
   );
 };
 
+// complete
 export default Header;
